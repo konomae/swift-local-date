addd3110ee316a5f8d6d1d1d39b86bb67d3cfb0b
konomae <konomae@users.noreply.github.com>
2021-05-03 20:00:54 +0900

    add public properties

--- Sources/LocalDate/LocalDate.swift
+++ Sources/LocalDate/LocalDate.swift
@@ -1,3 +1,11 @@
-struct LocalDate {
-    var text = "Hello, World!"
+public struct LocalDate {
+    public var year: Int
+    public var month: Int
+    public var day: Int
+    
+    public init(year: Int, month: Int, day: Int) {
+        self.year = year
+        self.month = month
+        self.day = day
+    }
 }
--- Tests/LocalDateTests/LocalDateTests.swift
+++ Tests/LocalDateTests/LocalDateTests.swift
@@ -1,11 +1,11 @@
-    import XCTest
-    @testable import LocalDate
+import XCTest
+@testable import LocalDate
 
-    final class LocalDateTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(LocalDate().text, "Hello, World!")
-        }
+final class LocalDateTests: XCTestCase {
+    func test_init() {
+        let date = LocalDate(year: 2021, month: 5, day: 1)
+        XCTAssertEqual(date.year, 2021)
+        XCTAssertEqual(date.month, 5)
+        XCTAssertEqual(date.day, 1)
     }
+}
