d00cf57142f4ab542052d0b753adfcdc97dd8442
konomae <konomae@users.noreply.github.com>
2021-05-05 15:48:55 +0900

    refactor tests

--- Tests/LocalDateTests/LocalDateTests.swift
+++ Tests/LocalDateTests/LocalDateTests.swift
@@ -3,34 +3,27 @@ import XCTest
 
 final class LocalDateTests: XCTestCase {
     func test_init() {
-        let date = LocalDate(year: 2021, month: 5, day: 1)
-        XCTAssertEqual(date.year, 2021)
-        XCTAssertEqual(date.month, 5)
+        let date = LocalDate(year: 1970, month: 1, day: 1)
+        XCTAssertEqual(date.year, 1970)
+        XCTAssertEqual(date.month, 1)
         XCTAssertEqual(date.day, 1)
     }
     
     func test_init_from_date_in_timeZone() {
-        var date = LocalDate(from: Date(timeIntervalSince1970: 0), in: TimeZone(secondsFromGMT: 0)!)
-        XCTAssertEqual(date.year, 1970)
-        XCTAssertEqual(date.month, 1)
-        XCTAssertEqual(date.day, 1)
+        XCTAssertEqual(
+            LocalDate(from: Date(timeIntervalSince1970: 0), in: TimeZone(secondsFromGMT: 0)!),
+            LocalDate(year: 1970, month: 1, day: 1)
+        )
         
-        date = LocalDate(from: Date(timeIntervalSince1970: 0), in: TimeZone(secondsFromGMT: -60)!)
-        XCTAssertEqual(date.year, 1969)
-        XCTAssertEqual(date.month, 12)
-        XCTAssertEqual(date.day, 31)
+        XCTAssertEqual(
+            LocalDate(from: Date(timeIntervalSince1970: 0), in: TimeZone(secondsFromGMT: -3600)!),
+            LocalDate(year: 1969, month: 12, day: 31)
+        )
     }
     
     func test_init_from_string() throws {
-        var date = try LocalDate(from: "1970-01-01")
-        XCTAssertEqual(date.year, 1970)
-        XCTAssertEqual(date.month, 1)
-        XCTAssertEqual(date.day, 1)
-        
-        date = try LocalDate(from: "1970/01/01")
-        XCTAssertEqual(date.year, 1970)
-        XCTAssertEqual(date.month, 1)
-        XCTAssertEqual(date.day, 1)
+        XCTAssertEqual(try LocalDate(from: "1970-01-01"), LocalDate(year: 1970, month: 1, day: 1))
+        XCTAssertEqual(try LocalDate(from: "1970/01/01"), LocalDate(year: 1970, month: 1, day: 1))
     }
     
     func test_init_from_string_error() {
@@ -52,23 +45,20 @@ final class LocalDateTests: XCTestCase {
     }
     
     func test_string() {
-        XCTAssertEqual(
-            LocalDate(year: 1970, month: 1, day: 1).string(),
-            "1970-01-01"
-        )
+        XCTAssertEqual(LocalDate(year: 1970, month: 1, day: 1).string(), "1970-01-01")
     }
     
     func test_init_from_decoder() throws {
-        let decoder = JSONDecoder()
-        let date = try decoder.decode(LocalDate.self, from: Data("\"1970-01-01\"".utf8))
-        XCTAssertEqual(date.year, 1970)
-        XCTAssertEqual(date.month, 1)
-        XCTAssertEqual(date.day, 1)
+        XCTAssertEqual(
+            try JSONDecoder().decode(LocalDate.self, from: Data("\"1970-01-01\"".utf8)),
+            LocalDate(year: 1970, month: 1, day: 1)
+        )
     }
     
     func test_encode_to_encoder() throws {
-        let encoder = JSONEncoder()
-        let data = try encoder.encode(LocalDate(year: 1970, month: 1, day: 1))
-        XCTAssertEqual(data, Data("\"1970-01-01\"".utf8))
+        XCTAssertEqual(
+            try JSONEncoder().encode(LocalDate(year: 1970, month: 1, day: 1)),
+            Data("\"1970-01-01\"".utf8)
+        )
     }
 }
