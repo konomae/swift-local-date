fc9ec0a6ba468b92321519cb113d320cfe9b2540
konomae <konomae@users.noreply.github.com>
2024-07-28 12:36:38 +0900

    use utf8 count

--- Sources/LocalDate/StringInterpolation.swift
+++ Sources/LocalDate/StringInterpolation.swift
@@ -1,7 +1,7 @@
 extension String.StringInterpolation {
     mutating func appendInterpolation(_ n: some SignedInteger, width: Int) {
         let description = abs(n).description
-        let s = String(repeating: "0", count: max(width - description.count, 0)) + description
+        let s = String(repeating: "0", count: max(width - description.utf8.count, 0)) + description
         appendLiteral(n < 0 ? "-" + s : s)
     }
 }
