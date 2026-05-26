5cb47c5ec8d4f234e1126fe16d332f7b4929ff69
konomae <konomae@users.noreply.github.com>
2026-05-26 22:47:38 +0900

    Drop Swift 5.x toolchain support (#98)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -37,5 +37,5 @@ let package = Package(
             ]
         ),
     ],
-    swiftLanguageVersions: [.version("6"), .v5]
+    swiftLanguageModes: [.v6]
 )
